fb2ab6460b6f3f9903767c44d8dff5f5bcc3a88a
elguetajj <elguetaj@ufm.edu>
2022-09-20 14:08:46 -0600

    changes for django 4.1 functionality

--- daterange_filter/filter.py
+++ daterange_filter/filter.py
@@ -12,7 +12,7 @@ from django import forms
 from django.contrib import admin
 from django.db import models
 from django.utils.translation import gettext as _
-from django.contrib.admin import static
+
 from django.conf import settings
 
 use_suit = 'DATE_RANGE_FILTER_USE_WIDGET_SUIT'
@@ -74,8 +74,8 @@ class DateRangeFilterBaseForm(forms.Form):
             css = ['widgets.css']
 
             return forms.Media(
-                js=[static("admin/js/%s" % path) for path in js],
-                css={'all': [static("admin/css/%s" % path) for path in css]}
+                js=[ ("admin/js/%s" % path) for path in js],
+                css={'all': ["admin/css/%s" % path for path in css]}
             )
 
 
